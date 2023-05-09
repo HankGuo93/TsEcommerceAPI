a431ff341823dc1c24174f3e2c02922d90ba5d68
HankGuo <52509416+HankGuo93@users.noreply.github.com>
2023-05-09 18:12:33 +0800

    feat : added getProductsByCategory API / updateProductById API

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -2,12 +2,27 @@ import express from 'express';
 
 import {
     getProducts as getProductsFromDb,
+    getProductsByCategory as getProductsByCategoryFromDb,
     createProduct as createProdcutFromDb,
+    updateProductById as updateProductByIdFromDb,
 } from '../db/product';
 
 export const getProducts = async (req: express.Request, res: express.Response) => {
     try {
         const products = await getProductsFromDb();
+        if (products === null) {
+            return res.sendStatus(404);
+        }
+        return res.status(200).json(products);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const getProductsByCategory = async (req: express.Request, res: express.Response) => {
+    try {
+        const products = await getProductsByCategoryFromDb(req.query.category.toString());
         if (products === null) {
             return res.sendStatus(404);
         }
@@ -28,6 +43,23 @@ export const createProduct = async (req: express.Request, res: express.Response)
             stock,
             category,
             createdAt: Math.floor(new Date().getTime() / 1000),
+        });
+        return res.status(200).json(product);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateProductById = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { name, description, price, category } = req.body;
+        const product = await updateProductByIdFromDb(id, {
+            name,
+            description,
+            price,
+            category
         });
         return res.status(200).json(product);
     } catch (error) {
--- src/router/product.ts
+++ src/router/product.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { createProduct, getProducts } from '../controllers/product';
+import { createProduct, getProducts, getProductsByCategory, updateProductById } from '../controllers/product';
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/product/all', getProducts);
+    router.get('/product', getProductsByCategory);
     router.post('/product', isAuthenticated, createProduct);
+    router.put('/product/:id', isAuthenticated, updateProductById);
 }
